9227756329e079aa517cac08ac6a1812f6de1f29
Matiasf <matiasf0308@gmail.com>
2020-08-25 22:52:45 -0300

    proyecto ecommerce

---
+++ my-app/src/components/navbar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './navbar.css';
+
+function Navbar() {
+    return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-warning">
+        <a classNameName="navbar-brand" href="#">Ecommerce</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Productos<span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Donde estamos</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Categorias
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="#">Action</a>
+                <a className="dropdown-item" href="#">Another action</a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contacto</a>
+            </li>
+          </ul>
+         
+        </div>
+      </nav>
+      
+    );
+  
+
+}
+
+
+export default Navbar;
+
